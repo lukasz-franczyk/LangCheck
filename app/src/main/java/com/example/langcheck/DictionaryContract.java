996daf6ddd31f67a684b66ea421f4e6320f97f46
Luk_Leon <lukasz.leon.franczyk@hotmail.com>
2023-02-23 00:54:09 +0100

    creating DictionaryContract.java file

---
+++ app/src/main/java/com/example/langcheck/DictionaryContract.java
@@ -0,0 +1,16 @@
+package com.example.langcheck;
+
+import android.provider.BaseColumns;
+
+public class DictionaryContract {
+
+    static final String TABLE_NAME = "Dictionary";
+
+    public static class Columns{
+        public static final String _ID = BaseColumns._ID;
+        public static final String DICTIONARY_ENGLISHNAME = "EnglishName";
+        public static final String DICTIONARY_POLISHNAME = "PolishName";
+        public static final String DICTIONARY_CODE = "Code";
+        public static final String DICTIONARY_ORIGINNAME = "OriginName";
+    }
+}
